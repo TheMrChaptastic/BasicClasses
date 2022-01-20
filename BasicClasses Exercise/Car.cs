613054875a62fb0a986c81e720c342f4173c2ad4
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-20 10:42:38 -0600

    first

---
+++ BasicClasses Exercise/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClasses_Exercise
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ BasicClasses Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClasses_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car muscle = new Car();
+            muscle.Make = "Ford";
+            muscle.Model = "Mustang";
+            muscle.Year = 2013;
+            Console.WriteLine($"{muscle.Year} {muscle.Make} {muscle.Model}");
+
+        }
+    }
+}
